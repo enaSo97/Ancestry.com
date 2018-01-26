da0feb4a33a88473749f3e356b9a5720f2420d97
ena so <yso02@uoguelph.ca>
2018-01-26 00:33:43 -0500

    bug fix

--- GEDCOM/src/GEDCOMhelper.c
+++ GEDCOM/src/GEDCOMhelper.c
@@ -229,7 +229,11 @@ Info tockenInfo(char * toParse){ //parses the line of GEDCOM and saves into temp
   strcpy(temp, toParse);
   //printf("parsing this line : %s\n", toParse);
 
-  while(strcmp((char*)temp[i + 1]," ") == 0){// iterating the first white space
+  for (int i = 0; i < 40; i++){
+    printf("each character %s\n", temp[i]);
+  }
+
+  /*while(strcmp((char*)temp[i + 1]," ") == 0){// iterating the first white space
     strcpy(level[i], temp[i]);
     i++;// iterates until it finds the first white space
   }
@@ -245,7 +249,7 @@ Info tockenInfo(char * toParse){ //parses the line of GEDCOM and saves into temp
   strcpy(info.level, level);
   strcpy(info.tag, tag);
   strcpy(info.info, information);
-  printf("[[%s %s %s]]\n", level, tag, information);
+  printf("[[%s %s %s]]\n", level, tag, information);*/
 
   return info;
 }
