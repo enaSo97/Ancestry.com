ab8a36835893272635311af9e667aebc8746de44
ena so <yso02@uoguelph.ca>
2018-01-28 00:01:59 -0500

    add head parser

--- GEDCOM/src/GEDCOMparser.c
+++ GEDCOM/src/GEDCOMparser.c
@@ -41,7 +41,7 @@ GEDCOMerror createGEDCOM(char* fileName, GEDCOMobject** obj){
     //printf("struct info <%s><%s><%s>\n", info[i].level, info[i].tag, info[i].info);
   }
   int i = 0;
-  while (i < length){// iterate till the end of the file
+  while (i < 30){// iterate till the end of the file
     int k = 0;
     if (strcmp(info[i].level, "0") == 0){//means it is the start of the record
       record = malloc(sizeof(Info));
