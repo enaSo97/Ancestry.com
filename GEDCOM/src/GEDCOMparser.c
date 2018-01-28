f2c8b169f247a8bd289fe231bfbfc4cbb159eb1a
ena so <yso02@uoguelph.ca>
2018-01-28 00:37:23 -0500

    add head parser

--- GEDCOM/src/GEDCOMparser.c
+++ GEDCOM/src/GEDCOMparser.c
@@ -41,7 +41,7 @@ GEDCOMerror createGEDCOM(char* fileName, GEDCOMobject** obj){
     //printf("struct info <%s><%s><%s>\n", info[i].level, info[i].tag, info[i].info);
   }
 
-  for (int i = 0; i < length; i++){
+  for (int i = 0; i < 30683; i++){
     int k = 0;
     int j = 1;
     printf("tracking i %d\n", i);
