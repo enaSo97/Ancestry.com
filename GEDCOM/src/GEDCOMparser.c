da9e28b8a4841282fe09371e66c992b8e3288765
ena so <yso02@uoguelph.ca>
2018-01-28 02:15:31 -0500

    add head parser

--- GEDCOM/src/GEDCOMparser.c
+++ GEDCOM/src/GEDCOMparser.c
@@ -41,7 +41,7 @@ GEDCOMerror createGEDCOM(char* fileName, GEDCOMobject** obj){
     //printf("struct info <%s><%s><%s>\n", info[i].level, info[i].tag, info[i].info);
   }
   int k = 0;
-  for (int i = 0; i < 43; i++){
+  /*for (int i = 0; i < 43; i++){
     int j = 1;
     printf("tracking i %d\n", i);
     if (strcmp(info[i].level, "0") == 0){
@@ -66,7 +66,7 @@ GEDCOMerror createGEDCOM(char* fileName, GEDCOMobject** obj){
     }
     i--;
     //i = k;
-  }
+  }*/
   length--;
   for (int i = 0; i < length; i++){ // freeing the allocated memory after done parsing the file
     //free(info[i]); // freeing the allocated information struct
