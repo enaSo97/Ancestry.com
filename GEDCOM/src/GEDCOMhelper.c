d66c674d8aa80c7fde891b5613277d6698e54c3e
enaSo97 <yso02@uoguelph.ca>
2018-01-30 18:21:51 -0500

    debug

--- GEDCOM/src/GEDCOMhelper.c
+++ GEDCOM/src/GEDCOMhelper.c
@@ -441,6 +441,7 @@ Header * headParser(Info * record, int length){
       }*/
     }
   }
+  printf("Source: %s, GED VER: %f, Encoding: %d", head->source, head->gedcVersion, head->encoding);
   char * print = toString(other);
   puts(print);
   return head;
