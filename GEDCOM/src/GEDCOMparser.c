7956e6eb5625ae426ac1202391b3cc5b6851e462
enaSo97 <yso02@uoguelph.ca>
2018-01-31 19:53:17 -0500

    debug

--- GEDCOM/src/GEDCOMparser.c
+++ GEDCOM/src/GEDCOMparser.c
@@ -251,7 +251,7 @@ char* printEvent(void* toBePrinted){ //type date place other fields
   length = strlen(temp->type) + strlen(temp->date) + strlen(temp->place) + 100;
   chrTemp = (char*)calloc(length, sizeof(char));
   sprintf(chrTemp, "type: %s, date: %s, place: %s\n", temp->type, temp->date, temp->place);
-  if (temp->otherFields != NULL){
+  if (temp->otherFields->next != NULL){
     strcat(chrTemp, toString(temp->otherFields));
   }
 
