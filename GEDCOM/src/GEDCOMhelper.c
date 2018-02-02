668d48583ccf5fceb88944c6f2ec57abe2414ac5
enaSo97 <yso02@uoguelph.ca>
2018-02-02 04:49:32 -0500

    debug

--- GEDCOM/src/GEDCOMhelper.c
+++ GEDCOM/src/GEDCOMhelper.c
@@ -651,7 +651,7 @@ Individual * parseIndividual(Info * record, int length, List * pointers, List*  
       temp->indiPoint = &person;
       //printf("\nchecking for indi pointer %s || %s || \n", temp->addr, temp->type);
       insertBack(pointers, temp);
-      printf("checking for pointer %s", (Pointer*)temp->indiPoint)
+      //printf("checking for pointer %s", (Pointer*)temp->indiPoint)
     }
     else if (validateIndividualEvent(record[i].tag) == 1){
       Event * event = calloc(1, sizeof(Event));
@@ -806,7 +806,7 @@ void linkerFunction(List * pointer, void * data){
       }
       else if(strcmp(set->type, "SUBM") == 0){
         printf("it's submitter\n");
-        set->subPoint = &((Pointer*)temp->data)->subPoint;
+        set->subPoint = ((Pointer*)temp->data)->subPoint;
         //printf("name: %s\n", ((Submitter*)set->subPoint)->submitterName);
         printf("set the submitter\n");
       }
