a2cf8abe7a6855804c73a5f17e7a527cd3376b67
enaSo97 <yso02@uoguelph.ca>
2018-02-02 03:16:32 -0500

    debug

--- GEDCOM/src/GEDCOMhelper.c
+++ GEDCOM/src/GEDCOMhelper.c
@@ -798,7 +798,7 @@ void linkerFunction(List * pointer, void * data){
     //point = (Pointer*)temp;
     char * string = returnString(((Pointer*)temp)->addr);
     printf("pointer %s || receiver %s||\n", ((Pointer*)temp->data)->addr, set->addr);
-    if (strcmp(((char*)temp->data)->addr, set->addr) == 0){
+    if (strcmp(((Pointer*)temp->data)->addr, set->addr) == 0){
       if (strcmp(set->type, "CHIL") == 0){
         printf("it's child\n");
         insertBack(childrenPtr, ((Pointer*)temp)->indiPoint);
