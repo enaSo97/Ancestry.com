c12d0a8eb590d3e241cc148ff6813468239fa6a2
enaSo97 <yso02@uoguelph.ca>
2018-02-01 19:13:54 -0500

    debug

--- GEDCOM/src/GEDCOMparser.c
+++ GEDCOM/src/GEDCOMparser.c
@@ -109,19 +109,19 @@ GEDCOMerror createGEDCOM(char* fileName, GEDCOMobject** obj){
 
   linkerFunction(&receiver, &pointers);
   length--;
-
+  /*
   printf("\nprinting individual\n");
   char * indi = toString(people);
   puts(indi);
-  printf("*********************************************\n");
+  printf("*********************************************\n");*/
 
-  /*char * print1 = toString(pointers);
+  char * print1 = toString(pointers);
   char * print2 = toString(receiver);
   printf("******pointers**************************\n");
   puts(print1);
   printf("\n******receivers**********************\n");
   puts(print2);
-  printf("*******************************************");*/
+  printf("*******************************************");
 
 
 
