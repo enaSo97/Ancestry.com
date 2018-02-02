79460a48f043883577933078c35dceeda5bcb680
enaSo97 <yso02@uoguelph.ca>
2018-02-02 10:24:28 -0500

    debug

--- GEDCOM/src/GEDCOMparser.c
+++ GEDCOM/src/GEDCOMparser.c
@@ -363,7 +363,10 @@ char* printIndividual(void* toBePrinted){
   other = toString(print->otherFields);
   puts(events);
   puts(other);
-  strcat(string, toString(print->events));
+  if (print->event != NULL){
+    strcat(string, toString(print->events));
+  }
+  //strcat(string, toString(print->events));
   strcat(string, toString(print->families));
   strcat(string, toString(print->otherFields));
   return string;
