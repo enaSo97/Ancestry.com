1bccc1c1c360da6656a34ca6c14519d2571cd781
enaSo97 <yso02@uoguelph.ca>
2018-02-02 21:31:13 -0500

    debug

--- GEDCOM/src/GEDCOMhelper.c
+++ GEDCOM/src/GEDCOMhelper.c
@@ -856,8 +856,8 @@ Family * parseFamily(Info * record, int length, List people){
         second3 = (Field*)field3->data;
         if(strcmp(kidRef[v], second3->value) == 0){
           printf("\n\n");
-          printIndividual(one3);
-          puts(one3);
+          char * kids = printIndividual(one3);
+          puts(kids);
           insertBack(&children, one3);
         //  printf("after insertbvack\n\n");
           v++;
