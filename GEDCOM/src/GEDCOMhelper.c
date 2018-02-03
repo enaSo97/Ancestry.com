3367cd7c78f3ff87aa177e75531bc57a1be92f19
enaSo97 <yso02@uoguelph.ca>
2018-02-02 21:00:37 -0500

    debug

--- GEDCOM/src/GEDCOMhelper.c
+++ GEDCOM/src/GEDCOMhelper.c
@@ -778,11 +778,12 @@ int validateFamilyEvent(char * check){
 
 Family * parseFamily(Info * record, int length, List people){
   Family * dummyFamily = calloc(1, sizeof(Family));
-  Individual * person;
-  Pointer * temp = calloc(1, sizeof(Pointer));
+  //Individual * person;
+  //Pointer * temp = calloc(1, sizeof(Pointer));
   //Field * field = calloc(1, sizeof(Field));
-  List other = initializeList(printField, deleteField, compareFields);
+  List other = initializeList(&printField, &deleteField, &compareFields);
   Field * extraStuff;
+  List children  = initializeList(&printIndividual, &deleteIndividual, &compareIndividuals);
   //List children = initializeList(printIndividual, deleteIndividual, compareIndividuals);
   //List events = initializeList(printEvent, deleteEvent, compareEvents);
   //List kids = initializeList(printIndividual, deleteIndividual, compareIndividuals);
@@ -833,6 +834,32 @@ Family * parseFamily(Info * record, int length, List people){
     }
     node2 = node2->next;
   }
+
+  for (int i = 0; i < length; i++){
+    if (strcmp(record[i].tag, "CHIL") == 0){
+      strcpy(xref, record[i].info);
+
+      Node * node3 = people.head;//
+      Individual * one3;
+      Node * field3;
+      Field * second3;
+
+      while(node3 != NULL){
+        one3 = (Individual*)node3->data;
+        field3 = one3->otherFields.head;
+        while(field3 != NULL){
+          second3 = (Field*)field3->data;
+          if(strcmp(xref, second3->value) == 0){
+            insertBack(&children, one3);
+            break;
+          }
+          field2 = field2->next;
+        }
+        node2 = node2->next;
+      }
+    }
+  }
+  dummyFamily->children = children;
 
   //char * wife = printIndividual(dummyFamily->wife);
   //puts(wife);
