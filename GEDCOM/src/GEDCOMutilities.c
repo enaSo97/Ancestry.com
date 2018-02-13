b112f534f87ea29cf59afb06976d15a13c723fb9
enaSo97 <yso02@uoguelph.ca>
2018-02-12 19:23:31 -0500

    fixing children

--- GEDCOM/src/GEDCOMutilities.c
+++ GEDCOM/src/GEDCOMutilities.c
@@ -842,9 +842,9 @@ Family * parseFamily(Info * record, int length, List people){
     for (int i = 0; i < length; i++){
       //k = 0;
       if (strcmp(record[i].tag, "CHIL") == 0){
-        printf("found children %s\n", record[i].info);
+        //printf("found children %s\n", record[i].info);
         strcpy(kidRef[k], record[i].info);
-        printf("saving to 2d array %s\n", record[i].info);
+        //printf("saving to 2d array %s\n", record[i].info);
         k++;
       }
     }
@@ -860,9 +860,9 @@ Family * parseFamily(Info * record, int length, List people){
           second3 = (Field*)field3->data;
           if(strcmp(kidRef[v], second3->value) == 0){
             printf("\n\n");
-            printf("found children %s || value in list %s\n",kidRef[v], second3->value);
-            char * kids = printIndividual(one3);
-            puts(kids);
+            //printf("found children %s || value in list %s\n",kidRef[v], second3->value);
+            //char * kids = printIndividual(one3);
+            //puts(kids);
             insertBack(&(dummyFamily->children), one3);
             //break;
           //  printf("after insertbvack\n\n");
@@ -872,9 +872,9 @@ Family * parseFamily(Info * record, int length, List people){
         node3 = node3->next;
       }
     }
-    printf("\n\n\n");
-    printf("length of chile %d\n", (dummyFamily->children).length);
-    puts(toString(dummyFamily->children));
+    //printf("\n\n\n");
+    //printf("length of chile %d\n", (dummyFamily->children).length);
+   // puts(toString(dummyFamily->children));
   //char * fam = printFamily(dummyFamily);
   //puts(fam);
   return dummyFamily;
